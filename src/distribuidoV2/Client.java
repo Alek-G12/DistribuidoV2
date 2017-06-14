f781198c6fa42d4de7cb9214e8d79187ef156bf4
Alek-G12 <Alek-G12@users.noreply.github.com>
2017-06-14 18:09:04 -0500

    changed displayed user

--- src/distribuidoV2/Client.java
+++ src/distribuidoV2/Client.java
@@ -51,6 +51,7 @@ public class Client implements Runnable {
                 //System.out.println("Server Address: " + svAddress);
 
                 String reply = System.getProperty("user.name");
+                reply = reply.concat(" @ " + InetAddress.getLocalHost().getHostName());
                 buf = reply.getBytes();
                 packet = new DatagramPacket(buf, buf.length, svAddress, 4445);
                 socket.send(packet);
